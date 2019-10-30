d26ca2e8fd016190d698f48de7f95d46fdc78c06
Raveesh Agarwal <hi@raveeshagarwal.in>
2019-10-30 20:40:43 +0530

    adds home screen buttons

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,100 @@ class HomeScreen extends StatelessWidget {
           'State Management Exps',
         ),
       ),
+      body: ListView(
+        children: <Widget>[
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+          HomeScreenSelectionButton(
+            buttonLabel: '00. setState ',
+            pushRouteName: '/setState',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// responsibility: provides styling for the HomeScreen buttons
+class HomeScreenSelectionButton extends StatelessWidget {
+  final String pushRouteName;
+  final String buttonLabel;
+
+  const HomeScreenSelectionButton({
+    Key key,
+    @required this.pushRouteName,
+    @required this.buttonLabel,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RaisedButton(
+        onPressed: () => Navigator.pushNamed(context, pushRouteName),
+        child: Text(
+          buttonLabel,
+          style: Theme.of(context)
+              .textTheme
+              .display1
+              .copyWith(color: Colors.black87),
+        ),
+        color: Colors.teal[100],
+        padding: EdgeInsets.symmetric(vertical: 25),
+      ),
     );
   }
 }
